ffdc98702901b70189460ad5e80b26111e60f1f0
shanghanrun <idim7@naver.com>
2024-04-11 15:29:23 +0900

    .

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -9,13 +9,16 @@ const MyPage = () => {
  
 
   return (
-	<div className="mypage-body">
-		<h1>MyPage</h1>
-		 <h3 style={{fontWeight:'bold'}}>제목 : {title}</h3>
-		<h3>예약 좌석</h3>
-		<div style={{borderBottom: '2px solid gray', width: '400px', marginBottom: '10px' }}></div>
-		<div>[ {seatList.join(', ')} ]</div>
-		
+	<div>
+		<div className="mypage-body" style={{ height:"50vh"}}>
+			<h1 style={{marginBottom:'20px'}}>MyPage</h1>
+			<h3 style={{fontWeight:'bold', 								marginBottom:'20px'}}>제목 : {title}</h3>
+			<h3>예약 좌석</h3>
+			<div style={{borderBottom: '2px solid gray', width: '400px', marginBottom: '10px' }}></div>
+			<div>[ {seatList.join(', ')} ]</div>
+			
+		</div>
+		<div style={{height:'20px'}}></div>
 	</div>
   )
 }
