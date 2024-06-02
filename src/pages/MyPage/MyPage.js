f90b44df34e418582b0338e14bfbb30d99b9ea1a
shanghanrun <idim7@naver.com>
2024-06-02 22:02:23 +0900

    .

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -18,7 +18,7 @@ const MyPage = () => {
 		// const url = `https://hm-shopping-full3.netlify.app`;
 		const movie ={title:title, seat:seat, image:image}
 		const movieInfo = encodeURIComponent(JSON.stringify(movie));
-		const url =`https://hm-shopping-full3.netlify.app/move?info=${movieInfo}`
+		const url =`https://hm-shopping-full3.netlify.app/gotoshopping?info=${movieInfo}`
 		window.location.href = url;
 	}
 
